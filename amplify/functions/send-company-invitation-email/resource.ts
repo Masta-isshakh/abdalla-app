dc1aadbf11ebfc70e868fb8db21d1d652a0ea27b
Masta <mastaisshakh@gmail.com>
2026-04-10 23:57:06 +0300

    abdalla

--- amplify/functions/send-company-invitation-email/resource.ts
+++ amplify/functions/send-company-invitation-email/resource.ts
@@ -2,6 +2,7 @@ import { defineFunction, secret } from '@aws-amplify/backend';
 
 export const sendCompanyInvitationEmail = defineFunction({
   name: 'send-company-invitation-email',
+  resourceGroupName: 'data',
   entry: './handler.ts',
   environment: {
     INVITATION_SENDER_EMAIL: secret('INVITATION_SENDER_EMAIL'),
